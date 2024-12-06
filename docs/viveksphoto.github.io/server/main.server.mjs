28b373112d53446d02655febd5362488c80c7a2c
Vivek <138993374+Indian-coding@users.noreply.github.com>
2024-12-06 18:25:55 +0530

    build

--- docs/viveksphoto.github.io/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-5TWM5NWB.mjs";import"./chunk-XOHUPSDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
---
+++ server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-2TAS3JHC.mjs";import"./chunk-XOHUPSDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
