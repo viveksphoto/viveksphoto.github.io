ca696ed5bef5d5d21b2e66a33a18b10c34ed0053
Vivek <138993374+Indian-coding@users.noreply.github.com>
2024-12-06 18:09:53 +0530

    structure updated

--- docs/server/main.server.mjs
+++
@@ -1,2 +0,0 @@
-import './polyfills.server.mjs';
-import{a}from"./chunk-EN4QIE3P.mjs";import"./chunk-XOHUPSDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
---
+++ docs/viveksphoto.github.io/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-5TWM5NWB.mjs";import"./chunk-XOHUPSDR.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
